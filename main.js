46c5715e8341a6ecd805e78ee4e285ea04dbb693
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-22 16:23:53 -0700

    in progress

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var data = {
+  facing: 'east',
+  started: false
+};
+
+var intervalId;
+
 window.addEventListener('keydown', turnCar);
 
 function turnCar() {
@@ -6,32 +13,44 @@ function turnCar() {
     case 'KeyW':
     case 'ArrowUp':
       car.setAttribute('class', 'car-up');
+      data.facing = 'north';
       break;
     case 'KeyS':
     case 'ArrowDown':
       car.setAttribute('class', 'car-down');
+      data.facing = 'south';
       break;
     case 'KeyA':
     case 'ArrowLeft':
       car.setAttribute('class', 'car-left');
+      data.facing = 'west';
       break;
     case 'KeyD':
     case 'ArrowRight':
       car.setAttribute('class', 'car-right');
+      data.facing = 'east';
       break;
   }
+
 }
 
-function startCar() {
-  var position = 0;
-  var car = document.querySelector('img');
-  if (event.keyCode === 32) {
-    setInterval(function () {
+function startAndStopCar() {
+
+  if (event.keyCode === 32 && data.started === false) {
+    // debugger;
+    intervalId = setInterval(function () {
+      var position = 0;
+      var car = document.querySelector('img');
       position += 10;
       car.style.left = position + 'px';
     }, 16);
+    data.started = true;
 
+  } else if (event.keyCode === 32 && data.started === true) {
+    clearInterval(intervalId);
+    data.started = false;
   }
+
 }
 
-window.addEventListener('keydown', startCar);
+window.addEventListener('keydown', startAndStopCar);
