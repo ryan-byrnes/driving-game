9674d52271ad54e5f8536dfb9cb6d93383ec8010
briossant <crosson.brieuc@gmail.com>
2021-07-23 14:48:30 +0200

    working moving car

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-var data = {
+const data = {
   facing: 'east',
   started: false,
   positionX: 0,
   positionY: 0
 };
 
-var intervalId;
 
-window.addEventListener('keydown', turnCar);
+const game_settings = {
+  background_image_size: 32, //px
+  car_speed: 10,
+  game_loop_speed: 16, //ms
+}
 
-function turnCar() {
-  var car = document.querySelector('img');
+
+let intervalId;
+const car = document.querySelector('img');
+
+
+window.addEventListener('keydown', aKeyIsDown);
+
+// add this function to use only one listener
+function aKeyIsDown(event) {
+
+  // stops the arrows from scrolling
+  event.preventDefault();
+  event.stopPropagation();
+
+  turnCar(event);
+  startAndStopCar(event);
+}
+
+
+function turnCar(event) {
   switch (event.code) {
     case 'KeyW':
     case 'ArrowUp':
@@ -33,24 +54,47 @@ function turnCar() {
       data.facing = 'east';
       break;
   }
-
 }
 
-function startAndStopCar() {
 
-  if (event.keyCode === 32 && data.started === false) {
-    intervalId = setInterval(function () {
-      var car = document.querySelector('img');
-      data.positionX += 10;
-      car.style.left = data.positionX + 'px';
-    }, 16);
+function startAndStopCar(event) {
+
+  // change .keyCode to .code
+  if (event.code === 'Space' && data.started === false) {
+
+    // move the interval function for easier reading
+    intervalId = setInterval(gameLoop, game_settings.game_loop_speed);
+
     data.started = true;
-
-  } else if (event.keyCode === 32 && data.started === true) {
+  } else if (event.code === 'Space' && data.started === true) {
     clearInterval(intervalId);
     data.started = false;
   }
 
 }
 
-window.addEventListener('keydown', startAndStopCar);
+
+const gameLoop = () => {
+
+  // move the car in the right direction
+  switch (data.facing) {
+    case "east":
+      data.positionX += game_settings.car_speed;
+      break;
+    case "west":
+      data.positionX -= game_settings.car_speed;
+      break;
+    case "north":
+      data.positionY -= game_settings.car_speed;
+      break;
+    case "south":
+      data.positionY += game_settings.car_speed;
+      break;
+  }
+
+  // move the background and not the car on the X axis
+  document.body.style.backgroundPositionX = `${data.positionX % game_settings.background_image_size}px`;
+  // move the background and not the car on the Y axis
+  document.body.style.backgroundPositionY = `${data.positionY % game_settings.background_image_size}px`;
+}
+
